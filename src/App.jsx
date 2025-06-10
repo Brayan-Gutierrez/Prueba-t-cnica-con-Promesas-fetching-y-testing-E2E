70b32af65e3ffd9b0efd7dc50c596b68870f4c22
BrayanGutierrez <br.desarrollador@gmail.com>
2025-06-10 06:09:29 -0600

    Install linter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export function App (){
+  const [fact, setFact] = useState("lorem ipsum cat fact whatever")
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+      <p>{fact}</p>
+    </main>
+  )
+}
